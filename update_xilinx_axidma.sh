a0e97c16043b3b644abc5f58622df26fdffd7357
46cv8 <58377031+46cv8@users.noreply.github.com>
2021-01-06 15:13:18 +0900

    added update script and fixed issue with duplicate import

---
+++ update_xilinx_axidma.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -a ./custom_modules/xilinx_axidma/driver/*.c ./custom_modules/xilinx_axidma/driver/*.h ./custom_modules/xilinx_axidma/include/axidma_ioctl.h ./project-spec/meta-user/recipes-modules/xilinx-axidma/files
+
